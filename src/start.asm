a5a0532f0d8e5fc1b69889dc43cf92d8e663696f
AKTuBHbIu_roMeC <tg @nordemef>
2024-03-21 20:32:52 +0200

    Rework reading of entry
    Remove copying of input to one heap,
    instead handle each entry right away

--- src/start.asm
+++ src/start.asm
@@ -8,30 +8,38 @@ section .text
     global _start
 
 _start:
-    lea rbx, [input]
+    lea rbx, [buf]
+    read_entry:
+        call read_line
+        add rbx, rax   ; move to the end
+        or rax, rax    ; number of bytes read
+        jnz read_entry
+    mov byte [rbx], 0x0
 
-read_next:
-    push rbx
+    jmp write_test
+    ;jmp exit
+
+read_line:
+    push rbp
+    mov rbp, rsp
     mov rax, 3		 ; read
+    mov rcx, rbx        ; address to pass to
+    push rbx
     mov rbx, 0		 ; standard input
-    mov rcx, buf        ; address to pass to
     mov rdx, [buf_size]		 ; input length 
     int 0x80
-    pop rbx               
-    or rax, rax 
-    jz proceed
+    pop rbx 
+    leave
+    ret
 
     ; save buf
-    mov rcx, rax
-    lea rsi, [buf]
-    lea rdi, [rbx]
-    cld
-    rep movsb
-    add rbx, rax
-    jmp read_next
-
-proceed:
-    mov byte [rbx], 0x0
+    ;mov rcx, rax
+    ;lea rsi, [buf]
+    ;lea rdi, [rbx]
+    ;cld
+    ;rep movsb
+    ;add rbx, rax
+    ;jmp read_line
 
 ascii_to_number:
     push rbp
@@ -76,16 +84,15 @@ upd_matrix_entry:
     leave
     ret
 
-; test
-write_next:
+write_test:
     mov rax, 4           ; write
-    mov rcx, input          ; address to the value
+    mov rcx, buf          ; address to the value
     sub rbx, rcx
     mov rdx, rbx           ; length of output 
-    push rbx
     mov rbx, 1           ; standard output
     int 0x80             
 
 exit:
     mov rax, 1
+    xor rbx, rbx
     int 0x80
